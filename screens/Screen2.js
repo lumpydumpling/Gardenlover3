8fcce5c2666a36ea23d1d4cc0d710321ceb15801
Chloe Au <93165494+lumpydumpling@users.noreply.github.com>
2023-04-30 08:18:31 -0700

    added more plants

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -5,10 +5,16 @@ const Screen2 = ({ navigation }) => {
     const [isPlant1Hovered, setIsPlant1Hovered] = useState(false);
     const [isPlant2Hovered, setIsPlant2Hovered] = useState(false);
     const [isPlant3Hovered, setIsPlant3Hovered] = useState(false);
+    const [isPlant4Hovered, setIsPlant4Hovered] = useState(false);
+    const [isPlant5Hovered, setIsPlant5Hovered] = useState(false);
+    const [isPlant6Hovered, setIsPlant6Hovered] = useState(false);
 
   const [plant1Position, setPlant1Position] = useState({ x: 0, y: 0 });
   const [plant2Position, setPlant2Position] = useState({ x: 200, y: 0 });
   const [plant3Position, setPlant3Position] = useState({ x: 100, y: 0 });
+  const [plant4Position, setPlant4Position] = useState({ x: 300, y: 0 });
+  const [plant5Position, setPlant5Position] = useState({ x: 100, y: 150 });
+  const [plant6Position, setPlant6Position] = useState({ x: 0, y: 150 });
 
   const panResponder1 = PanResponder.create({
     onMoveShouldSetPanResponder: () => true,
@@ -59,6 +65,55 @@ const Screen2 = ({ navigation }) => {
       },
   });
 
+  const panResponder4 = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gestureState) => {
+      setPlant4Position({
+        x: gestureState.moveX - 25, // subtract half of plant width to center it on cursor
+        y: gestureState.moveY - 25, // subtract half of plant height to center it on cursor
+      });
+    },
+    onPanResponderGrant: () => {
+        setIsPlant4Hovered(true);
+      },
+      onPanResponderRelease: () => {
+        setIsPlant4Hovered(false);
+      },
+  });
+
+  const panResponder5 = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gestureState) => {
+      setPlant5Position({
+        x: gestureState.moveX - 25, // subtract half of plant width to center it on cursor
+        y: gestureState.moveY - 25, // subtract half of plant height to center it on cursor
+      });
+    },
+    onPanResponderGrant: () => {
+        setIsPlant5Hovered(true);
+      },
+      onPanResponderRelease: () => {
+        setIsPlant5Hovered(false);
+      },
+  });
+
+
+  const panResponder6 = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gestureState) => {
+      setPlant6Position({
+        x: gestureState.moveX - 25, // subtract half of plant width to center it on cursor
+        y: gestureState.moveY - 25, // subtract half of plant height to center it on cursor
+      });
+    },
+    onPanResponderGrant: () => {
+        setIsPlant6Hovered(true);
+      },
+      onPanResponderRelease: () => {
+        setIsPlant6Hovered(false);
+      },
+  });
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 24 }}>Create your garden</Text>
@@ -87,6 +142,34 @@ const Screen2 = ({ navigation }) => {
           {...panResponder3.panHandlers}
         />
           {isPlant3Hovered && <Text style={{ fontSize: 16 }}>Daisies</Text>}
+
+    </View>
+      <Text style={{ fontSize: 24 }}>Create your garden</Text>
+      <View style={{ position: 'absolute', left: plant4Position.x, top: plant4Position.y }}>
+        <Animated.Image
+          source={require('../plant_images/ferns.jpg')}
+          style={{ width: 100, height: 100 }}
+          {...panResponder4.panHandlers}
+        />
+  {isPlant4Hovered && <Text style={{ fontSize: 16 }}>Fern plant</Text>}
+
+      </View>
+      <View style={{ position: 'absolute', left: plant5Position.x, top: plant5Position.y }}>
+        <Animated.Image
+          source={require('../plant_images/lavender.jpg')}
+          style={{ width: 100, height: 100 }}
+          {...panResponder5.panHandlers}
+        />
+        {isPlant5Hovered && <Text style={{ fontSize: 16 }}>Lavender</Text>}
+
+      </View>
+      <View style={{ position: 'absolute', left: plant6Position.x, top: plant6Position.y }}>
+        <Animated.Image
+          source={require('../plant_images/Olea_europaea.png')}
+          style={{ width: 100, height: 100 }}
+          {...panResponder6.panHandlers}
+        />
+          {isPlant6Hovered && <Text style={{ fontSize: 16 }}>Olea Europaea</Text>}
 
       </View>
       <Button title="Go back to Home" onPress={() => navigation.goBack()} 
