14ba58ec0544aae35c7c53728e62c8edc7d2cb03
Chloe Au <93165494+lumpydumpling@users.noreply.github.com>
2023-04-30 06:31:57 -0700

    added plant captions

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { View, Text, Button, PanResponder, Animated } from 'react-native';
 
 const Screen2 = ({ navigation }) => {
+    const [isPlant1Hovered, setIsPlant1Hovered] = useState(false);
+    const [isPlant2Hovered, setIsPlant2Hovered] = useState(false);
+    const [isPlant3Hovered, setIsPlant3Hovered] = useState(false);
+
   const [plant1Position, setPlant1Position] = useState({ x: 0, y: 0 });
   const [plant2Position, setPlant2Position] = useState({ x: 200, y: 0 });
   const [plant3Position, setPlant3Position] = useState({ x: 100, y: 0 });
@@ -14,6 +18,12 @@ const Screen2 = ({ navigation }) => {
         y: gestureState.moveY - 25, // subtract half of plant height to center it on cursor
       });
     },
+    onPanResponderGrant: () => {
+        setIsPlant1Hovered(true);
+      },
+      onPanResponderRelease: () => {
+        setIsPlant1Hovered(false);
+      },
   });
 
   const panResponder2 = PanResponder.create({
@@ -24,7 +34,14 @@ const Screen2 = ({ navigation }) => {
         y: gestureState.moveY - 25, // subtract half of plant height to center it on cursor
       });
     },
+    onPanResponderGrant: () => {
+        setIsPlant2Hovered(true);
+      },
+      onPanResponderRelease: () => {
+        setIsPlant2Hovered(false);
+      },
   });
+
 
   const panResponder3 = PanResponder.create({
     onMoveShouldSetPanResponder: () => true,
@@ -34,6 +51,12 @@ const Screen2 = ({ navigation }) => {
         y: gestureState.moveY - 25, // subtract half of plant height to center it on cursor
       });
     },
+    onPanResponderGrant: () => {
+        setIsPlant3Hovered(true);
+      },
+      onPanResponderRelease: () => {
+        setIsPlant3Hovered(false);
+      },
   });
 
   return (
@@ -45,6 +68,8 @@ const Screen2 = ({ navigation }) => {
           style={{ width: 100, height: 100 }}
           {...panResponder1.panHandlers}
         />
+  {isPlant1Hovered && <Text style={{ fontSize: 16 }}>Epipremnum plant</Text>}
+
       </View>
       <View style={{ position: 'absolute', left: plant2Position.x, top: plant2Position.y }}>
         <Animated.Image
@@ -52,6 +77,8 @@ const Screen2 = ({ navigation }) => {
           style={{ width: 100, height: 100 }}
           {...panResponder2.panHandlers}
         />
+        {isPlant2Hovered && <Text style={{ fontSize: 16 }}>Bird of Paradise plant</Text>}
+
       </View>
       <View style={{ position: 'absolute', left: plant3Position.x, top: plant3Position.y }}>
         <Animated.Image
@@ -59,6 +86,8 @@ const Screen2 = ({ navigation }) => {
           style={{ width: 100, height: 100 }}
           {...panResponder3.panHandlers}
         />
+          {isPlant3Hovered && <Text style={{ fontSize: 16 }}>Daisies</Text>}
+
       </View>
       <Button title="Go back to Home" onPress={() => navigation.goBack()} 
         color="green"
